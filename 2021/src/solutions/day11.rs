29073eaffccdc07beb1b618c09d45177c9b5af0b
H-Rusch <kirdneh98mail@gmail.com>
2022-11-07 13:47:58 +0100

    2021 day 11

---
+++ 2021/src/solutions/day11.rs
@@ -0,0 +1,112 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
+pub fn part1(input: &str) -> usize {
+    let mut matrix = parse(input);
+
+    let mut res = 0;
+    for _ in 0..100 {
+        res += step(&mut matrix);
+    }
+
+    res
+}
+
+pub fn part2(input: &str) -> usize {
+    let mut matrix = parse(input);
+    let size = matrix.iter().flatten().count();
+
+    let mut i = 0;
+    loop {
+        i += 1;
+        if step(&mut matrix) == size {
+            return i;
+        }
+    }
+}
+
+// change matrix in place and return number of flashes
+fn step(matrix: &mut Vec<Vec<u8>>) -> usize {
+    let width = matrix[0].len();
+    let height = matrix.len();
+
+    let mut flashing: Vec<(usize, usize)> = Vec::new();
+    let mut flashed: HashSet<(usize, usize)> = HashSet::new();
+
+    // charge up all squids
+    for (x, y) in (0..width).cartesian_product(0..height) {
+        matrix[y][x] += 1;
+        if matrix[y][x] == 10 {
+            flashing.push((x, y));
+        }
+    }
+
+    // flash all squids
+    while let Some((x, y)) = flashing.pop() {
+        flashed.insert((x, y));
+
+        for (dx, dy) in util::get_adjacent_with_diag(x, y, width, height) {
+            matrix[dy][dx] += 1;
+            if matrix[dy][dx] == 10 {
+                flashing.push((dx, dy));
+            }
+        }
+    }
+
+    // reset flashed squids
+    for &(x, y) in flashed.iter() {
+        matrix[y][x] = 0;
+    }
+
+    flashed.len()
+}
+
+fn parse(input: &str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect()
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_ex() {
+        let input = "\
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+        assert_eq!(part1(input), 1656);
+    }
+
+    #[test]
+    fn part2_ex() {
+        let input = "\
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+        assert_eq!(part2(input), 195);
+    }
+}
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -36,9 +36,44 @@ pub fn get_adjacent(
         (x < width - 1, (x + 1, y)),
     ]
     .into_iter()
-    .filter_map(|(cond, (dx, dy))| if cond { Some((dx as usize, dy as usize)) } else { None })
+    .filter_map(|(cond, (dx, dy))| {
+        if cond {
+            Some((dx as usize, dy as usize))
+        } else {
+            None
+        }
+    })
 }
 
+pub fn get_adjacent_with_diag(
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    let y = y as i32;
+    let x = x as i32;
+    let width = width as i32;
+    let height = height as i32;
+    [
+        (x > 0 && y > 0, (x - 1, y - 1)),
+        (x > 0, (x - 1, y)),
+        (x > 0 && y < height - 1, (x - 1, y + 1)),
+        (y > 0, (x, y - 1)),
+        (y < height - 1, (x, y + 1)),
+        (x < width - 1 && y > 0, (x + 1, y - 1)),
+        (x < width - 1, (x + 1, y)),
+        (x < width - 1 && y < height - 1, (x + 1, y + 1)),
+    ]
+    .into_iter()
+    .filter_map(|(cond, (dx, dy))| {
+        if cond {
+            Some((dx as usize, dy as usize))
+        } else {
+            None
+        }
+    })
+}
 
 #[cfg(test)]
 mod tests {
@@ -70,5 +105,22 @@ mod tests {
         let i = get_adjacent(0, 5, 10, 5);
 
         assert_eq!(2, i.count());
+    }
+
+    #[test]
+    fn diag_all() {
+        let left_edge = get_adjacent_with_diag(0, 2, 10, 10);
+        let right_edge = get_adjacent_with_diag(9, 2, 10, 10);
+        let middle = get_adjacent_with_diag(5, 5, 10, 10);
+        let corner = get_adjacent_with_diag(9, 9, 10, 10);
+        let top_edge = get_adjacent_with_diag(2, 0, 10, 10);
+        let bottom_edge = get_adjacent_with_diag(2, 9, 10, 10);
+
+        assert_eq!(left_edge.count(), 5);
+        assert_eq!(right_edge.count(), 5);
+        assert_eq!(middle.count(), 8);
+        assert_eq!(corner.count(), 3);
+        assert_eq!(top_edge.count(), 5);
+        assert_eq!(bottom_edge.count(), 5);
     }
 }
