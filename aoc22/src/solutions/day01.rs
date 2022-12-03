92231ec58c0399c30bf6c8fe166a727b0d02c3f7
H-Rusch <kirdneh98mail@gmail.com>
2022-12-03 16:03:14 +0100

    2022 day 1

--- aoc22/src/solutions/day01.rs
+++ aoc22/src/solutions/day01.rs
@@ -1,7 +1,56 @@
 pub fn part1(input: &str) -> usize {
-    0   
+    let loads = parse(input);
+
+    *loads.iter().max().unwrap()
 }
 
 pub fn part2(input: &str) -> usize {
-    0   
+    let mut loads = parse(input);
+    loads.sort();
+
+    loads.iter().rev().take(3).sum()
+}
+
+pub fn parse(input: &str) -> Vec<usize> {
+    input
+        .split("\n\n")
+        .map(|batch| {
+            batch
+                .lines()
+                .map(|line| line.parse::<usize>().unwrap())
+                .sum()
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+    #[test]
+    fn part1_ex() {
+        assert_eq!(24000, part1(INPUT));
+    }
+
+    #[test]
+    fn part2_ex() {
+        assert_eq!(45000, part2(INPUT));
+    }
 }
