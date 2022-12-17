a7193c2257c604fd608f89614aa2017e81763b63
H-Rusch <kirdneh98mail@gmail.com>
2022-12-17 12:21:35 +0100

    2022 day 16 part 1

--- aoc22/src/solutions/day16.rs
+++ aoc22/src/solutions/day16.rs
@@ -1,5 +1,4 @@
-use std::collections::{HashMap, VecDeque, HashSet};
-
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Debug)]
 struct Valve {
@@ -10,24 +9,51 @@ struct Valve {
 pub fn part1(input: &str) -> u32 {
     let valves = parse(input);
 
-    let navigation_map: HashMap<&str, HashMap<&str, u32>> = valves.keys().map(|&name| (name, build_navigation_map(name, &valves))).collect();
-    let opened: HashSet<&str> = navigation_map.keys().filter(|&key| valves.get(key).unwrap().flow_rate == 0).map(|key| *key).collect();
+    let navigation_map: HashMap<&str, HashMap<&str, u32>> = valves
+        .keys()
+        .map(|&name| (name, build_navigation_map(name, &valves)))
+        .collect();
+    let opened: HashSet<&str> = navigation_map
+        .keys()
+        .filter(|&key| valves.get(key).unwrap().flow_rate == 0)
+        .copied()
+        .collect();
     let mut cache = HashMap::new();
 
-    dfs("AA", 1, 0, 0, 30, &navigation_map, &valves, &opened, &mut cache)
+    dfs(
+        "AA",
+        1,
+        0,
+        0,
+        30,
+        &navigation_map,
+        &valves,
+        &opened,
+        &mut cache,
+    )
 }
 
 pub fn part2(_input: &str) -> usize {
     0
 }
 
-fn dfs<'a>(start: &'a str, time: u32, flow_rate: u32, released: u32, limit: u32, navigation_map: &'a HashMap<&str, HashMap<&str, u32>>, valves: &HashMap<&str, Valve>, opened: &HashSet<&str>, cache: &mut HashMap<(&'a str, u32, u32, u32), u32>) -> u32 {
+#[allow(clippy::too_many_arguments)]
+fn dfs<'a>(
+    start: &'a str,
+    time: u32,
+    flow_rate: u32,
+    released: u32,
+    limit: u32,
+    navigation_map: &'a HashMap<&str, HashMap<&str, u32>>,
+    valves: &HashMap<&str, Valve>,
+    opened: &HashSet<&str>,
+    cache: &mut HashMap<(&'a str, u32, u32, u32), u32>,
+) -> u32 {
     // early return if time exceeded
-    if time >= limit {
-        // println!("{time}, {released}, {flow_rate}");
-        return released - flow_rate * time.abs_diff(limit);
+    if time > limit {
+        return released - flow_rate * time.abs_diff(limit + 1);
     }
-    
+
     let state = (start, time, flow_rate, released);
     if let Some(value) = cache.get(&state) {
         return *value;
@@ -40,24 +66,28 @@ fn dfs<'a>(start: &'a str, time: u32, flow_rate: u32, released: u32, limit: u32,
     // update flow rate
     let new_flow_rate = flow_rate + valves.get(start).unwrap().flow_rate;
 
-    // save currently released amount
-    let mut max_release = released;
+    // spend 1 minute opening the valve
+    let released = released + new_flow_rate;
+    let time = time + 1;
 
     // all valves have been opened -> wait until the time runs out
     if opened.len() == valves.len() {
-        return released + new_flow_rate * time.abs_diff(limit);
+        return released + new_flow_rate * time.abs_diff(limit + 1);
     }
-    
-    for (name, cost) in navigation_map.get(start).unwrap() {
-        // connected valve is not already active
-        if !opened.contains(name) && time + cost <= limit {
+
+    let max_release = navigation_map
+        .get(start)
+        .unwrap()
+        .iter()
+        .filter(|(name, _)| !opened.contains(**name))
+        .map(|(name, cost)| {
             let next_time = time + cost;
             let next_released = released + new_flow_rate * cost;
 
-            // update the maximum released amount 
-            max_release = dfs(name, next_time, new_flow_rate, next_released, limit, navigation_map, valves, &opened, cache).max(max_release);
-        }
-    }
+            dfs(name, next_time, new_flow_rate, next_released, limit, navigation_map, valves, &opened, cache)
+        })
+        .max()
+        .unwrap();
 
     cache.insert(state, max_release);
 
@@ -95,10 +125,20 @@ fn parse(input: &str) -> HashMap<&str, Valve> {
         let (valve_part, tunnel_part) = line.split_once("; ").unwrap();
 
         let name = &valve_part[6..=7];
-        let flow_rate: u32 = valve_part[8..].strip_prefix(" has flow rate=").unwrap().parse().unwrap();
-        let leads_to: Vec<String> = tunnel_part.split_whitespace().skip(4).collect::<String>().split(',').map(|name| name.to_string()).collect();
+        let flow_rate: u32 = valve_part[8..]
+            .strip_prefix(" has flow rate=")
+            .unwrap()
+            .parse()
+            .unwrap();
+        let leads_to: Vec<String> = tunnel_part
+            .split_whitespace()
+            .skip(4)
+            .collect::<String>()
+            .split(',')
+            .map(|name| name.to_string())
+            .collect();
 
-        valves.insert(name, Valve { flow_rate, leads_to });
+        valves.insert(name,Valve { flow_rate, leads_to });
     }
 
     valves
