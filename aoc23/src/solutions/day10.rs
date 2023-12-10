01582a34a0205976bd345465a563d5626f147961
H-Rusch <kirdneh98mail@gmail.com>
2023-12-10 16:34:36 +0100

    2023 day 10 part 2

--- aoc23/src/solutions/day10.rs
+++ aoc23/src/solutions/day10.rs
@@ -10,13 +10,16 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    0
+    let (coordinates, start) = parse(input);
+    let circle = find_circle(start, &coordinates);
+
+    enclosed_area(&circle)
 }
 
-fn find_circle(start: Coordinate, coordinates: &HashMap<Coordinate, char>) -> String {
+fn find_circle(start: Coordinate, coordinates: &HashMap<Coordinate, char>) -> Vec<Coordinate> {
     let mut queue = init_queue(&start, coordinates);
 
-    while let Some((coordinate, direction, path)) = queue.pop_front() {
+    while let Some((coordinate, direction, mut path)) = queue.pop_front() {
         if coordinate == start {
             return path;
         }
@@ -24,10 +27,9 @@ fn find_circle(start: Coordinate, coordinates: &HashMap<Coordinate, char>) -> St
         let pipe = coordinates.get(&coordinate).unwrap();
         let next_direction = next_direction(*pipe, direction);
         let next_coordinate = coordinate.step(&next_direction, 1);
-        let mut next_path = path.clone();
-        next_path.push(*pipe);
+        path.push(next_coordinate);
 
-        queue.push_back((next_coordinate, next_direction, next_path));
+        queue.push_back((next_coordinate, next_direction, path));
     }
 
     unreachable!()
@@ -36,7 +38,7 @@ fn find_circle(start: Coordinate, coordinates: &HashMap<Coordinate, char>) -> St
 fn init_queue(
     start: &Coordinate,
     coordinates: &HashMap<Coordinate, char>,
-) -> VecDeque<(Coordinate, Direction, String)> {
+) -> VecDeque<(Coordinate, Direction, Vec<Coordinate>)> {
     [
         Direction::Up,
         Direction::Left,
@@ -49,7 +51,7 @@ fn init_queue(
         coordinates.contains_key(coordinate)
             && valid_directions(*coordinates.get(coordinate).unwrap(), direction)
     })
-    .map(|(coordinate, direction)| (coordinate, direction, String::from("S")))
+    .map(|(coordinate, direction)| (coordinate, direction, vec![coordinate]))
     .collect()
 }
 
@@ -84,6 +86,32 @@ fn valid_directions(pipe: char, direction: &Direction) -> bool {
     )
 }
 
+fn enclosed_area(circle: &[Coordinate]) -> usize {
+    let area = shoelace(circle);
+    internal_points(circle, area)
+}
+
+fn shoelace(polygon: &[Coordinate]) -> usize {
+    // Use the shoelace formula (https://en.wikipedia.org/wiki/Shoelace_formula) to calculate the area of the polygon.
+
+    let mut polygon: Vec<Coordinate> = polygon.to_vec();
+    polygon.push(polygon[0]);
+
+    let area: i32 = polygon
+        .iter()
+        .zip(polygon.iter().skip(1))
+        .map(|(p1, p2)| (p1.x * p2.y) - (p1.y * p2.x))
+        .sum();
+
+    area.unsigned_abs() as usize / 2
+}
+
+fn internal_points(polygon: &[Coordinate], area: usize) -> usize {
+    // Calculate number of inside points of the polygon defined though the coordinates with the Pick's Theorem (https://en.wikipedia.org/wiki/Pick%27s_theorem).
+
+    area - polygon.len() / 2 + 1
+}
+
 fn parse(input: &str) -> (HashMap<Coordinate, char>, Coordinate) {
     let mut coordinates = HashMap::new();
     for (y, row) in input.lines().enumerate() {
@@ -114,17 +142,22 @@ fn find_start(input: &str) -> Coordinate {
 mod tests {
     use super::*;
 
-    const EXAMPLE_1: &str = include_str!("../../examples/day10_1_1.txt");
-    const EXAMPLE_2: &str = include_str!("../../examples/day10_1_2.txt");
+    const EXAMPLE_1_1: &str = include_str!("../../examples/day10_1_1.txt");
+    const EXAMPLE_1_2: &str = include_str!("../../examples/day10_1_2.txt");
+    const EXAMPLE_2_1: &str = include_str!("../../examples/day10_2_1.txt");
+    const EXAMPLE_2_2: &str = include_str!("../../examples/day10_2_2.txt");
+    const EXAMPLE_2_3: &str = include_str!("../../examples/day10_2_3.txt");
 
     #[test]
     fn part1_ex() {
-        assert_eq!(4, part1(EXAMPLE_1));
-        assert_eq!(8, part1(EXAMPLE_2));
+        assert_eq!(4, part1(EXAMPLE_1_1));
+        assert_eq!(8, part1(EXAMPLE_1_2));
     }
 
     #[test]
     fn part2_ex() {
-        assert_eq!(0, part2(""));
+        assert_eq!(4, part2(EXAMPLE_2_1));
+        assert_eq!(8, part2(EXAMPLE_2_2));
+        assert_eq!(10, part2(EXAMPLE_2_3));
     }
 }
